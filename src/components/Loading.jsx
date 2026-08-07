6000ac821d109924321e83297045f5f714d239fb
anishdubey176 <anishdubey655@gmail.com>
2026-08-07 13:44:09 +0530

    style: change loading logo text to AD

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -46,7 +46,7 @@ const Loading = ({ percent }) => {
     <>
       <div className="loading-header">
         <a href="/#" className="loader-title" data-cursor="disable">
-          Logo
+          AD
         </a>
         <div className={`loaderGame ${clicked && "loader-out"}`}>
           <div className="loaderGame-container">
